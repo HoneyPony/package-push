d97d7381313fa8c8b40bc85d71016f0a150ef6e3
Honey Pony <honeyponygames@gmail.com>
2020-10-04 15:52:59 -0400

    Fix up tutorial

--- Foam.gd
+++ Foam.gd
@@ -4,6 +4,12 @@ var initial_target = null
 
 func _ready():
 	override_puzzle_entity = true
+	
+	var tex = floor(rand_range(0, 2.95))
+	if tex == 1:
+		$Sprite.texture = load("res://vector/CreateFoam2.png")
+	elif tex == 2:
+		$Sprite.texture = load("res://vector/CreateFoam3.png")
 
 func _process(delta):
 	if speed.length() > 0:
